b837f641fa0584cd2532636c9c9c620713c66e68
Vipreet-Buddhi <siddharthv2106@gmail.com>
2025-07-19 20:02:01 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Portfolio/', // 👈 Important!
+  base: "/sid-portfolio", // 👈 Important!
   plugins: [react()],
 })
